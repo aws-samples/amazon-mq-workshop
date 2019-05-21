936fea6ab34c27464c9e2ec50e942bfb5aa2c9ca
Christian Mueller <cmr@amazon.de>
2019-05-21 10:46:32 +0200

    upgrade to the latest Amazon MQ version and libraries

--- CloudFormationTemplate/ec2-script-c9-docker.sh
+++ CloudFormationTemplate/ec2-script-c9-docker.sh
@@ -4,17 +4,17 @@
 echo "Starting Amazon Workshop Script ..."
 cd ~
 
-
-
 sudo amazon-linux-extras install docker -y
 sudo service docker start
 
 mkdir /home/ec2-user/workspace
 cd ~/workspace
+
 # Copy the clients used for the labs
-wget https://s3.amazonaws.com/amazon-mq-workshop/labs-clients.zip
-unzip labs-clients.zip
-rm labs-clients.zip
+wget https://s3.amazonaws.com/amazon-mq-workshop/amazon-mq-client.jar
+wget https://s3.amazonaws.com/amazon-mq-workshop/stomp-client.jar
+wget https://s3.amazonaws.com/amazon-mq-workshop/mqtt-client.jar
+wget https://s3.amazonaws.com/amazon-mq-workshop/amqp-client.jar
 chmod +x *.jar
 
 cd ~
