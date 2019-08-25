2555e21240c4e7fb13e84dde6fff1a5f53ec27af
Sam Andaluri <andsamba@amazon.com>
2019-08-25 22:50:13 +0000

    minor tweaks. set java_home automatically

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 cd ~/environment
 wget https://d3pxv6yz143wms.cloudfront.net/8.222.10.1/java-1.8.0-amazon-corretto-devel-1.8.0_222.b10-1.x86_64.rpm
 sudo yum localinstall -y java-1.8.0-amazon-corretto-devel-1.8.0_222.b10-1.x86_64.rpm
+echo "export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which javac))))" >> ~/.bashrc
 wget http://mirror.cc.columbia.edu/pub/software/apache/maven/maven-3/3.6.1/binaries/apache-maven-3.6.1-bin.tar.gz
 tar zxvf apache-maven-3.6.1-bin.tar.gz
 echo "export PATH=~/environment/apache-maven-3.6.1/bin:$PATH" >> ~/.bashrc
@@ -11,9 +12,9 @@ mkdir ~/environment/activemq-perftest/reports
 userPassword=`aws ssm get-parameter --name "MQBrokerUserPassword" | grep Value`
 brokerUser=`echo $userPassword | sed 's/"//g' | sed 's/Value://g' | cut -d',' -f1 | sed 's/ //g'`
 brokerPassword=`echo $userPassword | sed 's/"//g' | sed 's/Value://g' | cut -d',' -f2`
-printf "factory.brokerURL=$url\n" >> ~/environment/amazon-mq-workshop/openwire-producer.properties
+printf "\nfactory.brokerURL=$url\n" >> ~/environment/amazon-mq-workshop/openwire-producer.properties
 printf "factory.userName=$brokerUser\n" >> ~/environment/amazon-mq-workshop/openwire-producer.properties
 printf "factory.password=$brokerPassword\n" >> ~/environment/amazon-mq-workshop/openwire-producer.properties
-printf "factory.brokerURL=$url\n" >> ~/environment/amazon-mq-workshop/openwire-consumer.properties
+printf "\nfactory.brokerURL=$url\n" >> ~/environment/amazon-mq-workshop/openwire-consumer.properties
 printf "factory.userName=$brokerUser\n" >> ~/environment/amazon-mq-workshop/openwire-consumer.properties
 printf "factory.password=$brokerPassword\n" >> ~/environment/amazon-mq-workshop/openwire-consumer.properties
